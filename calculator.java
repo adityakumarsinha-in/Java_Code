f7e7bd51a00b14c109c42433065497f617cf4656
Aditya Kumar Sinha <94775186+adityakumarsinha-in@users.noreply.github.com>
2026-05-18 20:05:26 +0530

    Add files via upload

--- calculator.java
+++ calculator.java
@@ -1,32 +1,47 @@
 
-import java.util.Scanner;
+import java.util.*;
 
-public class calculator {
+class calculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        char ch = sc.next().charAt(0);
-        switch (ch) {
-            case '+':
-                System.out.println(a + b);
+
+        int ans = 0;
+        while (true) {
+            System.out.print("Enter the operator: ");
+            char op = sc.next().trim().charAt(0);
+
+            System.out.print("Enter two numbers:  ");
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                int num1 = sc.nextInt();
+                int num2 = sc.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    if (num2 != 0) {
+                        ans = num1 / num2;
+                    }
+                }
+
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
                 break;
-            case '-':
-                System.out.println(a - b);
-                break;
-            case '*':
-                System.out.println(a * b);
-                break;
-            case '/':
-                System.out.println(a / b);
-                break;
-            default:
-                System.out.println("Invalid operator");
-                break;
+            } else {
+                System.out.println("Invlid operation!");
+            }
+
+            System.out.println(ans);
         }
-
     }
-
-)
 }
